635f9459383b5f32d546fe107ddeae4b195f2b3c
ibrahimturdu <ibrahimturdu@gmail.com>
2023-06-07 11:13:12 +0300

    first commit

---
+++ jscode.js
@@ -0,0 +1,13 @@
+// var emailInput = document.getElementById("emailInput");
+// // var emailValidationIcon = document.getElementById("emailValidationIcon");
+
+// emailInput.addEventListener("input", function() {
+//   var email = emailInput.value;
+// //   var isValidEmail = /\S+@\S+\.\S+/.test(email); // Basit bir email doğrulama kontrolü
+
+//   if (emailInput) {
+//     emailInput.style.display = "block";
+//   } else {
+//     emailInput.style.display = "none";
+//   }
+// });
